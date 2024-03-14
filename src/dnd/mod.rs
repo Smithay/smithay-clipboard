1879b30d495a4734a526b0f5780cbf6ebc72e241
Ashley Wulber <ashley@system76.com>
2024-03-14 16:28:26 -0400

    feat: add conversion from string for MimeType

---
+++ src/dnd/mod.rs
@@ -0,0 +1,14 @@
+use crate::Clipboard;
+
+impl Clipboard {
+    /// Set up DnD operations for the Clipboard
+    pub fn init_dnd() {}
+
+    /// Start a DnD operation on the given surface with some data
+    pub fn start_dnd() {}
+
+    /// End the current DnD operation, if there is one
+    pub fn end_dnd() {}
+
+    
+}

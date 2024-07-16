0009cbfcf2a1dd3f9c5afb654185b9b5012a6344
kchibisov <kchibisov@users.noreply.github.com>
2024-07-16 17:59:39 +0000

    deploy: 49096ab16b2378c07b7f224620e54789624769d4

--- trait.impl/core/marker/trait.Freeze.js
+++ trait.impl/core/marker/trait.Freeze.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"smithay_clipboard":[["impl Freeze for <a class=\"struct\" href=\"smithay_clipboard/struct.Clipboard.html\" title=\"struct smithay_clipboard::Clipboard\">Clipboard</a>",1,["smithay_clipboard::Clipboard"]]]
+"smithay_clipboard":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"smithay_clipboard/struct.Clipboard.html\" title=\"struct smithay_clipboard::Clipboard\">Clipboard</a>",1,["smithay_clipboard::Clipboard"]]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
